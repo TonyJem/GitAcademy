ac05b87f6a8aea183287312eeb0c1d510dfa8716
TonyJem <a.jemanov@gmail.com>
2021-06-15 15:53:11 +0300

    Create Login button

--- GitAcademy/GitAcademy/ViewController.swift
+++ GitAcademy/GitAcademy/ViewController.swift
@@ -2,8 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loginButton.backgroundColor = .blue
+        loginButton.layer.cornerRadius = 30
+        loginButton.setTitleColor(.white, for: .normal)
+    }
+    
+    @IBAction func loginButtonTap(_ sender: UIButton) {
+        print("🟢 LoginButton Did Tap")
     }
 }
