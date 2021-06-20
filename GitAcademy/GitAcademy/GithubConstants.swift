279151ea09e4add147de01dbbc1e8806dda0d5e8
TonyJem <a.jemanov@gmail.com>
2021-06-20 21:44:13 +0300

    Create GithubConstants struct

---
+++ GitAcademy/GitAcademy/GithubConstants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct GithubConstants {
+    static let CLIENT_ID = "92b35e71026b016762ca"
+    static let CLIENT_SECRET = "e1c04ab5a751b7ad5eb8f565b8110cf2aa943083"
+    static let REDIRECT_URI = "https://github.com/TonyJem/GitAcademy"
+    static let SCOPE = "read:user,user:email"
+    static let TOKENURL = "https://github.com/login/oauth/access_token"
+}
