7da5a6fef7d5f0948301f4a6dc9225b3bc796d6e
TonyJem <a.jemanov@gmail.com>
2021-06-21 21:37:07 +0300

    Change App Credentials

--- GitAcademy/GitAcademy/GithubConstants.swift
+++ GitAcademy/GitAcademy/GithubConstants.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 struct GithubConstants {
-    static let CLIENT_ID = "92b35e71026b016762ca"
-    static let CLIENT_SECRET = "e1c04ab5a751b7ad5eb8f565b8110cf2aa943083"
-    static let REDIRECT_URI = "https://github.com/TonyJem/GitAcademy"
+    static let CLIENT_ID = "Iv1.8f06ae8264fe2a88"
+    static let CLIENT_SECRET = "2ad37abd3777f54c63dd34ae756ea1cb2f69dc87"
+    static let REDIRECT_URI = "authhub://oauth-callback"
     static let SCOPE = "read:user"
     static let TOKENURL = "https://github.com/login/oauth/access_token"
 }
