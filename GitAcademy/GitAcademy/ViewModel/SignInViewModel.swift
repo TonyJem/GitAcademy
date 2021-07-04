9c9160615d2c75f5e8e25cc7786a0e2bfcf69e0f
TonyJem <a.jemanov@gmail.com>
2021-07-04 11:52:41 +0300

    Update SignInViewModel.swift

--- GitAcademy/GitAcademy/ViewModel/SignInViewModel.swift
+++ GitAcademy/GitAcademy/ViewModel/SignInViewModel.swift
@@ -45,14 +45,14 @@ class SignInViewModel: NSObject {
             print("🔴 Failed to start ASWebAuthenticationSession")
         }
     }
-   
+    
     /* TODO: Find out if need it: in case yes - use it; no - delete this block
-    func appeared() {
-        // Try to get the user in case the tokens are already stored on this device
-        getUser()
-    }
-    */
-
+     func appeared() {
+     // Try to get the user in case the tokens are already stored on this device
+     getUser()
+     }
+     */
+    
 }
 
 //MARK: - Private
