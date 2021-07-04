ecb12f9d4e83c44aee0b901d36d2354551e237c4
TonyJem <a.jemanov@gmail.com>
2021-07-04 16:58:24 +0300

    Insert custom cells in profileTableView

--- GitAcademy/GitAcademy/ProfileViewController.swift
+++ GitAcademy/GitAcademy/ProfileViewController.swift
@@ -1,11 +1,69 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet private weak var profileTableView: UITableView!
+    
+    private let numberOfSections = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileTableView.register(UINib(nibName: String(describing: ProfileCell.self), bundle: Bundle.main), forCellReuseIdentifier: String(describing: ProfileCell.self))
+        
+        profileTableView.register(UINib(nibName: String(describing: RepositoryCell.self), bundle: Bundle.main), forCellReuseIdentifier: String(describing: RepositoryCell.self))
+        
+        profileTableView.dataSource = self
+        profileTableView.delegate = self
+        profileTableView.tableFooterView = UIView()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ProfileViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return numberOfSections
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Profile"
+        case 1:
+            return "Repositories"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            // TODO: Magic number
+            return 1
+        case 1:
+            // TODO: Magic number
+            return 2
+        default:
+            return .zero
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let profileCell = profileTableView.dequeueReusableCell(withIdentifier: String(describing: ProfileCell.self), for: indexPath) as? ProfileCell else { return UITableViewCell() }
+        
+        guard let repositoryCell = profileTableView.dequeueReusableCell(withIdentifier: String(describing: RepositoryCell.self), for: indexPath) as? RepositoryCell else { return UITableViewCell() }
+        
+        switch indexPath.section {
+        case 0:
+            return profileCell
+        case 1:
+            return repositoryCell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate {
 }
