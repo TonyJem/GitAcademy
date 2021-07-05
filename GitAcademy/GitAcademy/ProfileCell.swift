392aba949003fa035b84bc11bdf61d5f28233498
TonyJem <a.jemanov@gmail.com>
2021-07-05 17:05:11 +0300

    Add shadow to Avatar container

--- GitAcademy/GitAcademy/ProfileCell.swift
+++ GitAcademy/GitAcademy/ProfileCell.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ProfileCell: UITableViewCell {
     @IBOutlet private weak var contanerView: UIView!
+    @IBOutlet private weak var avatarContainerView: UIView!
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var loginLabel: UILabel!
@@ -9,11 +10,21 @@ class ProfileCell: UITableViewCell {
     @IBOutlet private weak var followingButton: UIButton!
     @IBOutlet private weak var followButton: UIButton!
     
+    private let shadowOffset = CGSize(width: 10, height: 10)
+    private let shadowOpacity: Float = 0.4
+    private let shadowRadius: CGFloat = 8
+    private let cornerRadius: CGFloat = 10
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        contanerView.roundCorners(radius: 10)
+        contanerView.roundCorners(radius: cornerRadius)
         contanerView.setShadow()
+        avatarImageView.roundCorners()
+        avatarContainerView.roundCorners()
+        avatarContainerView.setShadow(offsetSize: shadowOffset,
+                                      opacity: shadowOpacity,
+                                      radius: shadowRadius)
     }
     
     //    MARK: - Actions
@@ -28,12 +39,11 @@ class ProfileCell: UITableViewCell {
     @IBAction func followButtonAction(_ sender: UIButton) {
         print("🟢 Follow Button Did Tap")
     }
-
-//    MARK: - Methods
+    
+    //    MARK: - Methods
     func fillContent() {
         // TODO: Find better sollution how to inject User data without using Core
         let user = Core.profile.user
-        avatarImageView.roundCorners()
         avatarImageView.downloaded(from: user.avatar_url)
         nameLabel.isHidden = user.name == nil
         nameLabel.text = user.name
