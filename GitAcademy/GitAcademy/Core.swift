78ad85b9de890d919d63a7140709089e009cd528
TonyJem <a.jemanov@gmail.com>
2021-07-06 21:43:30 +0300

    Add more TODOs in comments

--- GitAcademy/GitAcademy/Core.swift
+++ GitAcademy/GitAcademy/Core.swift
@@ -1,4 +1,6 @@
-//TODO: Remove testAvatar image from assets while not needed
+// TODO: Find out if it is necessary to wathc out when Token is expiring and refresh it before;
+// TODO: Prevent fetching pictures if it is already was loaded, may be is possible to use some kind of frameworks to chash it;
+// TODO: Remove testAvatar image from assets while not needed;
 
 import Foundation
 
