22009f19c8302fc86c007fc351b1871cd22ddfd4
TonyJem <a.jemanov@gmail.com>
2021-07-07 22:57:48 +0300

    Add repositoriesTableView into RepositoryViewController

--- GitAcademy/GitAcademy/RepositoryViewController.swift
+++ GitAcademy/GitAcademy/RepositoryViewController.swift
@@ -1,10 +1,31 @@
 import UIKit
 
 class RepositoryViewController: UIViewController {
-
+    
+    @IBOutlet private weak var repositoriesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        repositoriesTableView.backgroundColor = .systemGray5
+        repositoriesTableView.dataSource = self
+        repositoriesTableView.delegate = self
+        repositoriesTableView.tableFooterView = UIView()
     }
+}
+
+extension RepositoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 88
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
+
+extension RepositoryViewController: UITableViewDelegate {
     
 }
