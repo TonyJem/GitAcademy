07a5632235e2d4b1d298a3a40aaa3c728da798d6
TonyJem <a.jemanov@gmail.com>
2021-07-08 00:04:45 +0300

    Create Outlets and actions for RepositoryDetailsCell

--- GitAcademy/GitAcademy/RepositoryDetailsCell.swift
+++ GitAcademy/GitAcademy/RepositoryDetailsCell.swift
@@ -1,7 +1,15 @@
 import UIKit
 
 class RepositoryDetailsCell: UITableViewCell {
-
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var repositoryName: UILabel!
+    @IBOutlet private weak var repositoryDescription: UILabel!
+    @IBOutlet private weak var starsButton: UIButton!
+    @IBOutlet private weak var languageIconView: UIView!
+    @IBOutlet private weak var languageLabel: UILabel!
+    @IBOutlet private weak var contributorsButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -12,5 +20,13 @@ class RepositoryDetailsCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    @IBAction func starsButtonAction(_ sender: UIButton) {
+        print("🟢 starsButton did Tap")
+    }
+    
+    @IBAction func contributorsButtonAction(_ sender: UIButton) {
+        print("🟢 contributorsButton did Tap")
     }
 }
