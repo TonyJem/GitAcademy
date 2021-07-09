5a31e109aed2058d328ac680a99ee426a8fe4f49
TonyJem <a.jemanov@gmail.com>
2021-07-09 16:57:56 +0300

    Update APIManager with fetchStarred functionality

--- GitAcademy/GitAcademy/API/APIError.swift
+++ GitAcademy/GitAcademy/API/APIError.swift
@@ -1,4 +1,8 @@
 enum APIError: Error {
+    case failedRequest
+    case unexpectedDataFormat
+    case failedURLCreation
+    case failedAuthentication
     case invalidResponse
     case otherError
 }
--- GitAcademy/GitAcademy/API/APIManager.swift
+++ GitAcademy/GitAcademy/API/APIManager.swift
@@ -1,4 +1,37 @@
 import Foundation
 
 struct APIManager {
+    
+    private var session: URLSession {
+        let configuration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration)
+    }
+}
+
+extension APIManager {
+    
+    func fetchStarred(_ completion: @escaping (Result<[Starred], APIError>) -> ()) {
+        guard let url = APIEndpoint.starred.url else {
+            completion(.failure(.failedURLCreation))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        session.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    do {
+                        let episodes = try JSONDecoder().decode([Starred].self, from: data)
+                        completion(.success(episodes))
+                    } catch {
+                        completion(.failure(.unexpectedDataFormat))
+                    }
+                } else {
+                    completion(.failure(.failedRequest))
+                }
+            }
+        }.resume()
+    }
 }
