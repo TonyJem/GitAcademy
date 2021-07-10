38dcdbac205d395104317c1d1fa85e7ada73e062
TonyJem <a.jemanov@gmail.com>
2021-07-10 22:35:44 +0300

    Update RepositoryDetailsCell.swift

--- GitAcademy/GitAcademy/RepositoryDetailsCell.swift
+++ GitAcademy/GitAcademy/RepositoryDetailsCell.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class RepositoryDetailsCell: UITableViewCell {
-    @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var repositoryNameLabel: UILabel!
     @IBOutlet private weak var repositoryDescriptionLabel: UILabel!
     @IBOutlet private weak var starsButton: UIButton!
@@ -19,7 +19,14 @@ class RepositoryDetailsCell: UITableViewCell {
     }
     
     func fillContent(for repository: Repository) {
+        usernameLabel.text = repository.owner.username
+        avatarImageView.downloaded(from: repository.owner.avatarURL)
         repositoryNameLabel.text = repository.name
         repositoryDescriptionLabel.text = repository.description
+        let starsButtonTitle = String(repository.stargazersCount)
+        starsButton.setTitle(starsButtonTitle, for: .normal)
+        // TODO: Set color accodingly to language
+        languageIconView.backgroundColor = .purple
+        languageLabel.text = repository.language
     }
 }
