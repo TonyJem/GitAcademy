4eee94629e79f8f73ca5c570fbe9ffdf22e11ead
TonyJem <a.jemanov@gmail.com>
2021-07-10 23:15:11 +0300

    Improve RepositoryDetailsCell UI

--- GitAcademy/GitAcademy/RepositoryDetailsCell.swift
+++ GitAcademy/GitAcademy/RepositoryDetailsCell.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class RepositoryDetailsCell: UITableViewCell {
+    @IBOutlet private weak var containerView: UIView!
     @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var avatarContainerView: UIView!
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var repositoryNameLabel: UILabel!
     @IBOutlet private weak var repositoryDescriptionLabel: UILabel!
@@ -9,6 +11,17 @@ class RepositoryDetailsCell: UITableViewCell {
     @IBOutlet private weak var languageIconView: UIView!
     @IBOutlet private weak var languageLabel: UILabel!
     @IBOutlet private weak var contributorsButton: UIButton!
+    
+    private let cornerRadius: CGFloat = 10
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        containerView.roundCorners(radius: cornerRadius)
+        containerView.setShadow()
+        avatarImageView.roundCorners()
+        avatarContainerView.roundCorners()
+    }
     
     @IBAction func starsButtonAction(_ sender: UIButton) {
         print("🟢 starsButton did Tap")
