0d60e6676d813aee96c8a442f0741d20aefd5bb2
TonyJem <a.jemanov@gmail.com>
2021-07-11 09:50:35 +0300

    Create 'hexStringToUIColor' functionality for RepositoryDetailsCell

--- GitAcademy/GitAcademy/RepositoryDetailsCell.swift
+++ GitAcademy/GitAcademy/RepositoryDetailsCell.swift
@@ -40,7 +40,32 @@ class RepositoryDetailsCell: UITableViewCell {
         let starsButtonTitle = String(repository.stargazersCount)
         starsButton.setTitle(starsButtonTitle, for: .normal)
         // TODO: Set color accodingly to language
-        languageIconView.backgroundColor = .purple
+        languageIconView.backgroundColor = hexStringToUIColor(hex: "#ffac45")
         languageLabel.text = repository.language
+    }
+}
+
+// MARK: - Private Methods
+private extension RepositoryDetailsCell {
+    func hexStringToUIColor (hex: String) -> UIColor {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        guard cString.count != 6 else {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
